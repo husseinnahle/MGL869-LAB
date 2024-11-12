9942c87873cd1ed1e6a7cb614032ffe757815ed9
Marie2023 <12888813-mariepier2023@users.noreply.gitlab.com>
2024-11-12 12:17:40 -0500

    Ajout de la boucle sur les versions pour sortir tous les fichiers .csv

--- src/mgl869_lab.py
+++ src/mgl869_lab.py
@@ -1,13 +1,17 @@
 from pathlib import Path
 import csv
 import os
-
+import json
 from modules.jira_extractor import extract_issues
 from modules.commit_extractor_localDirectory import get_commits
 
-# JIRA_SEARCH_FILTER = "project = HIVE AND issuetype = Bug AND status in (Resolved, Closed) AND resolution = FIXED AND affectedVersion ~ \"2.*\""
-JIRA_SEARCH_FILTER = "project = HIVE AND issuetype = Bug AND status in (Resolved, Closed) AND resolution = Fixed AND affectedVersion = 2.0.0"
-OUTPUT_FILE = Path("Bugs_2.0.0.csv")  # Extraire les commits dans ce fichier
+file_path = Path('modules/ReleaseVersion_Commit.json')
+
+
+def load_release_versions(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data
 
 
 def write_commits_to_csv(commit_list, output_file):
@@ -27,19 +31,26 @@ def write_commits_to_csv(commit_list, output_file):
                 writer.writerow([key, bug_file])
 
 
-print("Extracting jira issues...")
-issues = {}
-for issue in extract_issues(JIRA_SEARCH_FILTER, ["versions"]):
-    issues[issue["key"]] = {"affectedVersions": [e["name"] for e in issue["fields"]["versions"]]}
-print(f"\tFound {len(issues)} issues.\n")
+release_versions = load_release_versions(file_path)
 
-# Note: Hive doit être sur la main branch pour le code bonne les bons résultats
-print("Extracting github commits...")
-repo_path = r'C:\Users\lafor\Desktop\ETS - Cours\MGL869-01_Sujets speciaux\Laboratoire\Hive\hive'
-print(repo_path)
-commits_list = get_commits(repo_path, issues)
+for version in release_versions:
+    JIRA_SEARCH_FILTER = "project = HIVE AND issuetype = Bug AND status in (Resolved, Closed) AND resolution = Fixed AND affectedVersion = " + version
+    OUTPUT_FILE_NAME = "Bugs_" + version + ".csv"
+    OUTPUT_FILE = Path(OUTPUT_FILE_NAME)  # Extraire les commits dans ce fichier
 
-write_commits_to_csv(commits_list, OUTPUT_FILE)
+    print("Extracting jira issues...")
+    issues = {}
+    for issue in extract_issues(JIRA_SEARCH_FILTER, ["versions"]):
+        issues[issue["key"]] = {"affectedVersions": [e["name"] for e in issue["fields"]["versions"]]}
+    print(f"\tFound {len(issues)} issues.\n")
 
-del commits_list
-del issues
+    # Note: Hive doit être sur la main branch pour que le code bonne les bons résultats
+    print("Extracting github commits...")
+    repo_path = r'C:\Users\lafor\Desktop\ETS - Cours\MGL869-01_Sujets speciaux\Laboratoire\Hive\hive'
+    print(repo_path)
+    commits_list = get_commits(repo_path, issues)
+
+    write_commits_to_csv(commits_list, OUTPUT_FILE)
+
+    del commits_list
+    del issues
