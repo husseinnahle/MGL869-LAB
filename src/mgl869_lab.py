9ba523de35c644480df57f7b7f6b147f0146fba1
Marie2023 <12888813-mariepier2023@users.noreply.gitlab.com>
2024-11-12 11:48:43 -0500

    Modification de la méthode des bugs + version des commits

--- src/mgl869_lab.py
+++ src/mgl869_lab.py
@@ -1,101 +1,45 @@
-from collections import defaultdict
 from pathlib import Path
-import threading
-import time
-import schedule
+import csv
+import os
 
 from modules.jira_extractor import extract_issues
-from modules.commit_extractor import extract_commit_content, extract_commits, extract_tags
+from modules.commit_extractor_localDirectory import get_commits
 
-JIRA_SEARCH_FILTER = "project = HIVE AND issuetype = Bug AND status in (Resolved, Closed) AND resolution = FIXED AND affectedVersion ~ \"2.*\""
-OUTPUT_FILE = Path("commits.json")  # Extraire les commits dans ce fichier
+# JIRA_SEARCH_FILTER = "project = HIVE AND issuetype = Bug AND status in (Resolved, Closed) AND resolution = FIXED AND affectedVersion ~ \"2.*\""
+JIRA_SEARCH_FILTER = "project = HIVE AND issuetype = Bug AND status in (Resolved, Closed) AND resolution = Fixed AND affectedVersion = 2.0.0"
+OUTPUT_FILE = Path("Bugs_2.0.0.csv")  # Extraire les commits dans ce fichier
 
-def _extract_commits(issues: list[str]) -> list[dict]:
-	"""Extract commits by issues.
 
-	Args:
-		issues (list[str]): List of JIRA issue keys.
+def write_commits_to_csv(commit_list, output_file):
+    # Check if the file exists
+    if os.path.exists(output_file):
+        # Delete the file if it exists
+        os.remove(output_file)
+        print(f"Deleted existing file: {output_file}")
 
-    Returns:
-        list[dict]: List of dictionnaries containning:
-            - key (str): Jira key.
-            - commit (str): Commit ID.
-			- modifiedFiles (list[str]): List of modified filenames.
-	"""
-	def _print_commit_count():
-		print(f"\tFound {round(len(found)*100/len(issues), 2)}% commits.")
+    with open(output_file, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(['key', 'filename'])
 
-	def _start_scheduler():
-		schedule.every(10).seconds.do(_print_commit_count)
-		while not stop_event.is_set():
-			schedule.run_pending()
-			time.sleep(1)
-
-	stop_event = threading.Event()
-	scheduler_thread = threading.Thread(target=_start_scheduler)
-	scheduler_thread.start()
-	found = {}
-	max_tries = 20
-	min_found = int(0.9 * len(issues))
-	try_count = 0
-	previous_found_count = 0
-	page = 1
-	per_page = 100
-	commits = extract_commits(page=page, per_page=per_page)
-	while commits or len(found) != len(issues):
-		for commit in commits:
-			message = commit["message"]
-			for ticket in issues:
-				if ticket in message:
-					commit["files"] = extract_commit_content(commit["sha"])
-					found[ticket] = commit
-					break
-		if len(found) == previous_found_count and len(found) > min_found:
-			try_count += 1
-			if try_count >= max_tries:
-				not_found = set(issues) - set(found.keys())
-				print(f"\tMax tries reached... {len(issues) - len(found)} ticket(s) not found: {','.join(not_found)}")
-				break
-		else:
-			try_count = 0
-			previous_found_count = len(found)
-		page += 1
-		commits = extract_commits(page=page, per_page=100)
-	stop_event.set()
-	scheduler_thread.join()
-	return [
-		{
-			"key": issue_key,
-			"commit": commit["sha"],
-			"modifiedFiles": commit["files"]
-		}
-		for issue_key, commit in found.items()
-	]
+        for commit in commit_list:
+            key = commit['key']
+            for bug_file in commit['files']:
+                writer.writerow([key, bug_file])
 
 
 print("Extracting jira issues...")
 issues = {}
 for issue in extract_issues(JIRA_SEARCH_FILTER, ["versions"]):
-	issues[issue["key"]] = {"affectedVersions": [e["name"] for e in issue["fields"]["versions"]]}
+    issues[issue["key"]] = {"affectedVersions": [e["name"] for e in issue["fields"]["versions"]]}
 print(f"\tFound {len(issues)} issues.\n")
 
+# Note: Hive doit être sur la main branch pour le code bonne les bons résultats
 print("Extracting github commits...")
-commits = _extract_commits(issues.keys())
-print(f"\tFound {len(commits)} commits.\n")
+repo_path = r'C:\Users\lafor\Desktop\ETS - Cours\MGL869-01_Sujets speciaux\Laboratoire\Hive\hive'
+print(repo_path)
+commits_list = get_commits(repo_path, issues)
 
-files_with_bugs = defaultdict(set)
-for commit in commits:
-    files = commit.get("modifiedFiles", [])
-    if not files:
-        continue
-    affected_versions = issues[commit["key"]].get("affectedVersions", [])
-    for version in affected_versions:
-        files_with_bugs[version].update(files)
+write_commits_to_csv(commits_list, OUTPUT_FILE)
 
-del commits
+del commits_list
 del issues
-
-# import re
-# pattern = re.compile("(rel/)?release-2\\.[0-9]\\.0$")
-# tags = [e for e in extract_tags() if re.match(pattern, (e["tagName"]))]
-# print(tags)
