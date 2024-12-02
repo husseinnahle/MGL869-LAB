8ce04a6fa7d9eafa33ea85191c319f6ce234ef0f
augusto <augusto.latge@erudit.org>
2024-12-01 19:44:59 -0500

    Adjust model

--- src/generate_models.py
+++ src/generate_models.py
@@ -1,13 +1,6 @@
 from src.modules.model import generate_model
 
-versions = [
-    {"current_version": "2_0_0", "previous_version": None},
-    {"current_version": "2_1_0", "previous_version": "2_0_0"},
-    {"current_version": "2_2_0", "previous_version": "2_1_0"},
-    {"current_version": "2_3_0", "previous_version": "2_2_0"},
-    {"current_version": "3_0_0", "previous_version": "2_3_0"},
-    {"current_version": "3_1_0", "previous_version": "3_0_0"},
-]
+versions = ["2_0_0", "2_1_0", "2_2_0", "2_3_0", "3_0_0", "3_1_0"]
 
 for version in versions:
-    generate_model(version["current_version"], version["previous_version"])
+    generate_model(version)
